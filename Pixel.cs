bbe6cf997b554ac977caff44b95eae0ddc7557b7
StShoo <61821643+StShoo@users.noreply.github.com>
2023-02-26 16:54:45 +0200

    Created repo started the project

---
+++ Pixel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_consoleGameProject
+{
+    public readonly struct Pixel
+    {
+        public Pixel(int x, int y, ConsoleColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+        public int X { get; }
+        public int Y { get; }
+        public ConsoleColor Color { get; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Snake_consoleGameProject
+﻿using static System.Console;
+namespace Snake_consoleGameProject
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            SetWindowSize(MapWidth, MapHeight);
+            SetBufferSize(MapWidth, MapHeight);
+            CursorVisible = false;
+
+            ReadKey();
         }
     }
 }
